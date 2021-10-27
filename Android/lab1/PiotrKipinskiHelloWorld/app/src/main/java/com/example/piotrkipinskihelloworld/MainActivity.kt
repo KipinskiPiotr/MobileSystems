8b9328159895eec66a69b2bfc18dcb9ce43bbaea
KipinskiPiotr <49042374+KipinskiPiotr@users.noreply.github.com>
2021-10-27 19:40:14 +0200

    BMI layout

--- Android/lab1/PiotrKipinskiHelloWorld/app/src/main/java/com/example/piotrkipinskihelloworld/MainActivity.kt
+++ Android/lab1/PiotrKipinskiHelloWorld/app/src/main/java/com/example/piotrkipinskihelloworld/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.calculator_layout)
+        setContentView(R.layout.bmi_layout)
     }
 }
